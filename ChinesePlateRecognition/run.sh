6ca06121555613ed28f5e10e8ff969e7f4419382
Guo Haria <hariag@users.noreply.github.com>
2017-06-11 00:49:15 -0500

    Create run.sh

--- ChinesePlateRecognition/run.sh
+++ ChinesePlateRecognition/run.sh
@@ -1,2 +1,5 @@
-xhost +
-nvidia-docker run --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged=true --device /dev/nvidia-uvm:/dev/nvidia-uvm --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl -it mxnet/python:gpu /bin/bash
+#!/bin/sh
+sudo apt-get -y install x11-xserver-utils
+xhost +local:root
+nvidia-docker run --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged=true --device /dev/nvidia-uvm:/dev/nvidia-uvm --device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl -it guoharia/platerecognition:gpu /bin/bash
+xhost -local:root
